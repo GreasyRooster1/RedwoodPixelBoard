23d615fbb73ea4daac6324b834e65e6f1aebb9fc
GreasyRooster <greasyRooster1@gmail.com>
2025-05-15 09:40:05 -0700

    log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use simplelog::*;
+use log::info;
+
 fn main() {
-    println!("Hello, world!");
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("logs/logs.log").unwrap()),
+        ]
+    ).unwrap();
+    info!("Hello, world!");
 }
