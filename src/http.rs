7c6462867ab41d6148c7f55229438c96b73cefda
GreasyRooster <greasyRooster1@gmail.com>
2025-05-15 09:56:46 -0700

    responding with file

--- src/http.rs
+++ src/http.rs
@@ -1,6 +1,10 @@
+use std::ffi::OsStr;
 use std::fmt::format;
+use std::fs::File;
+use std::path::{Component, Path, PathBuf};
+use std::str::FromStr;
 use log::{debug, error, info};
-use rouille::{Request, Response, router};
+use rouille::{extension_to_mime, Request, Response, router};
 
 pub fn start_http(port:u32){
     rouille::start_server(format!("0.0.0.0:{port}"), move |request| {
@@ -17,12 +21,31 @@ pub fn start_http(port:u32){
                     Response::
                 },
 
-                (GET) (/stats) => {
-                    panic!("Not implemented yet")
-                },
-
-                _ => rouille::Response::empty_404()
+                _ => {
+                    let req_path = request.url()
+                    if request.method() == "GET" {
+                        debug!("{} {} {} {} requested file read",request.remote_addr(), request.method(), request.raw_url(),request.header("Host").unwrap());
+                        let path = match get_path(req_path){
+                            Ok(p) => p
+                            Err(_) => {
+                                return Response::empty_404();
+                            }
+                        }
+                        let extension = Path::new(&path).extension().and_then(OsStr::to_str).unwrap();
+                        let file = File::open(path).unwrap();
+                        return Response::from_file(extension_to_mime(extension),file)
+                    }
+                    Response::empty_404()
+                }
             )
         })
     });//,cert,pkey).unwrap().run();
+}
+
+fn get_path(uri:String)->Result<String,String>{
+    let path = PathBuf::from_str(format!("./data/{uri}").as_str()).unwrap();
+    if path.components().any(|x| x == Component::ParentDir) {
+        return Err("directory traversal".to_string());
+    }
+    Ok(path.as_path().to_str().unwrap().to_string())
 }
