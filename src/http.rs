074b4960ab9e6909c709cfc4b287926be60ecf29
GreasyRooster <greasyRooster1@gmail.com>
2025-05-16 11:19:21 -0700

    no

--- src/http.rs
+++ src/http.rs
@@ -4,14 +4,14 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::{Component, Path, PathBuf};
 use std::str::FromStr;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use log::{debug, error, info};
 use rouille::{extension_to_mime, Request, Response, router};
 use rand::{distributions::Alphanumeric, Rng}; // 0.8
 
 const NOT_FOUND_CONTENT:&str = include_str!("../404.html");
 pub fn start_http(port:u32){
-    let queue_mutex:Mutex<Vec<String>> = Mutex::new(vec![]);
+
     rouille::start_server(format!("0.0.0.0:{port}"), move |request| {
         let log_ok = |req: &Request, resp: &Response, _elap: std::time::Duration| {
             info!("{} {} {} {}", request.remote_addr(), req.method(), req.raw_url(),req.header("Host").unwrap());
@@ -19,6 +19,7 @@ pub fn start_http(port:u32){
         let log_err = |req: &Request, _elap: std::time::Duration| {
             error!("{} Handler panicked: {} {}", request.remote_addr(), req.method(), req.raw_url());
         };
+        let queue_mutex:Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
         rouille::log_custom(request, log_ok, log_err,  || {
             router!(request,
                 (GET) (/) => {
@@ -28,7 +29,7 @@ pub fn start_http(port:u32){
 
 
                 (POST) (/api/queue_image) => {
-                    queue_image(request,queue_mutex)
+                    queue_image(request,queue_mutex);
                     Response::empty_204()
                 },
 
@@ -43,13 +44,14 @@ pub fn start_http(port:u32){
     });//,cert,pkey).unwrap().run();
 }
 
-fn queue_image(request: &Request, queue_mutex:Mutex<Vec<String>>){
+fn queue_image(request: &Request, queue_mutex:Arc<Mutex<Vec<String>>>){
     let s: String = rand::thread_rng()
         .sample_iter(&Alphanumeric)
         .take(16)
         .map(char::from)
         .collect();
-    let path = format!("./queue/{0}-{1}",request.remote_addr(),s);
+    let name = format!("{0}-{1}",request.remote_addr(),s);
+    let path = format!("./queue/{name}");
     let bytes = request.data().unwrap().bytes();
     let mut file:File = File::create(&path).unwrap();
 
@@ -58,6 +60,9 @@ fn queue_image(request: &Request, queue_mutex:Mutex<Vec<String>>){
         file.write(&[byte.unwrap()]).expect("failed to write");
     }
 
+    let mut queue = queue_mutex.lock().unwrap();
+    debug!("{path} {queue:#?}");
+    queue.push(name);
 }
 
 fn resolve_get(request: &Request,req_path:String) ->Response{
